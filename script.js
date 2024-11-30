f062ebf97e4820e8e8db192e021874f64590fc5f
marcia-gmarques <marciagm00@gmail.com>
2024-11-30 19:38:14 +0100

    add css

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-user_name = document.getElementbyID("name");
-console.log(user_name);
+//document.write("Hello");
+const userName = document.getElementById('name');
+const submitButton = document.getElementById("submit_button");
+
+//adds on click listener event to do something when button is clicked
+submitButton.addEventListener("click", function(){
+    const name = userName.value;
+
+    if(name != null){
+        displayName = document.createElement('p');
+        displayName.textContent = name;
+        document.body.appendChild(displayName);
+    }
+    // console.log(name);
+    // console.log(displayName);
+
+});
