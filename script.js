35e4166f109e779f2e49ad1bd1afbb8afa47d8f3
marcia-gmarques <marciagm00@gmail.com>
2024-12-01 12:51:33 +0100

    dog images and code for status variables

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-//document.write("Hello");
-const userName = document.getElementById('name');
+//initialise ids from document
+const petName = document.getElementById('name');
 const submitButton = document.getElementById("submit_button");
 const intro = document.getElementById("intro");
-const content = document.getElementById("content");
-content.style.display = 'none';
 
 //adds on click listener event to do something when button is clicked
 submitButton.addEventListener("click", function(){
-    const name = userName.value;
+    const name = petName.value;
 
     if(name != null){
         displayName = document.createElement('p');
-        displayName.textContent = "Welcome " + name;
+        displayName.textContent = "You  got a new puppy called " + name + "!";
+        //display it on the dom
         intro.appendChild(displayName);
+
+        //make the input box and submit button disappear 
+        petName.style.display = "none";
+        submitButton.style.display = "none";
     }
-    // console.log(name);
-    // console.log(displayName);
 
 });
+
+//initialise variables for health, hunger and happiness
+let happiness = 50;
+let health = 50;
+let hunger = 80; //higher is worse??
+
+//iniitialise action buttons
+const feed = document.getElementById("feed_button");
+const pet = document.getElementById("pet_button");
+const treat = document.getElementById("treat_button");
+const sleep = document.getElementById("sleep_button");
+
+//initialise array with images of dog
+let dogMoods = ["/virtual_pet_character/dog_happy.png", "/virtual_pet_character/dog_mad.png", "/virtual_pet_character/dog_normal.png",
+                "/virtual_pet_character/dog_sad.png", "/virtual_pet_character/dog_sleeping.png", "/virtual_pet_character/dog_surprised.png"]
+
+//adds on click listener event to do something when button is clicked
+feed.addEventListener("click", function(){
+    hunger -= 20;
+    happiness += 5;
+    health += 20;
+
+    //check values of variables after clicking feed button
+    console.log("happiness", happiness);
+    console.log("health", health);
+    console.log("hunger", hunger);
+});
+
+//check initial values of the variables
+console.log("happiness", happiness);
+console.log("health", health);
+console.log("hunger", hunger);
